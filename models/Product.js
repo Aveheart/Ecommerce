0210a6272b050dad3c719d0f31cee4be639401d5
Averi <averi.sherman@gmail.com>
2022-09-28 22:28:45 -0400

    Tag model and fixed some errors in other models

--- models/Product.js
+++ models/Product.js
@@ -29,7 +29,7 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 1,
+      defaultValue: 10,
       validate: {
         isNumeric: true
       }

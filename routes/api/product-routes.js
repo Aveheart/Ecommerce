c232909aa3c975e514fb699cdf6d270d9c87b323
Averi <averi.sherman@gmail.com>
2022-09-30 00:25:30 -0400

    fixed error in product route for get by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,10 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
 try {
   const productData = await Product.findByPk(req.params.id, {
-    include: [{ model: Category, attributes:['category_name'] }, { model: Tag, attributes:['tag_name']}]
+    include: [{ model: Category, attributes:['category_name']}, 
+    { model: Tag, attributes:['tag_name']}]
  });
- if (!singleProduct) {
+ if (!productData) {
   res.status(404).json({ message: 'No product found by this ID'});
   return;
  }
